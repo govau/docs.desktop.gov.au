54be44650471dc421d12bac69193cb61a19fdaa0
Michael Tran <michael@dublue.com>
2020-09-14 15:43:36 +1000

    Option 4

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,21 +1,7 @@
 $( document ).ready( function() {
-    $('html').removeClass('no-js').addClass('js');
-    $('table:not(.au-table)').addClass('au-table');
-    $('thead:not(.au-table__head)').addClass('au-table__head');
-    $('tbody:not(.au-table__body)').addClass('au-table__body');
-    $('tr:not(.au-table__row)').addClass('au-table__row');
-    $('th:not(.au-table__header)').addClass('au-table__header');
-    $('td:not(.au-table__cell)').addClass('au-table__cell');
-
+    $('table:not(.table)').addClass('table table-bordered');
 
     if ( $('#toc-container > ul > li').length > 0 ) {
-        /*
-        if ($('#sidebar-nav-container > ul.au-link-list > li.active').length > 0)
-            $('#sidebar-nav-container > ul.au-link-list > li.active').append( $('div#toc-container > ul.au-link-list') );
-        else
-            $('div#toc-container').addClass('hidden');
-        */
-
         $('#back-to-top a').on( 'click', function() {
             $('html, body').animate({
                     scrollTop: 0
@@ -29,19 +15,10 @@ $( document ).ready( function() {
                     $('#back-to-top').removeClass('visible');
             }
         });
-
-        $('#toc-container').prepend(
-            '<h2 class="au-sidenav__title">On this page</h2>',
-            $('ul#markdown-toc').addClass('au-link-list')
-        )
-        .scrollspy({
-            offset: -25
-        })
-        .affix({
-            offset: {
-                top: $('header.au-header').outerHeight(true) + 100,
-            }
-        });
     }
 
+    $('[data-spy="scroll"]').each(function () {
+        var $spy = $(this).scrollspy('refresh')
+        console.log($spy);
+      });
 });
---
+++ platform.md
@@ -0,0 +1,6 @@
+---
+layout: page
+title: Platform
+---
+
+Content
